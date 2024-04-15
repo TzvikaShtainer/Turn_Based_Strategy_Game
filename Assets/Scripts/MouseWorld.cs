bd9902d630375483a37644398611fb8f1cb1b25a
TzvikaShtainer <TzvikaShtainer@gmail.com>
2024-04-15 18:05:44 +0300

    Move Unit To Mouse Pos

---
+++ Assets/Scripts/MouseWorld.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWorld : MonoBehaviour
+{
+    private static MouseWorld instance;
+    
+    [SerializeField] private LayerMask mousePointLayerMask;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
+
+    public static Vector3 GetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit rayCastHit, float.MaxValue, instance.mousePointLayerMask);
+        return rayCastHit.point;
+    }
+}
