3e25afe1495f155c28d9add46cf5ea431b71692d
TzvikaShtainer <TzvikaShtainer@gmail.com>
2024-04-16 10:18:44 +0300

    Adding Animation For Walking

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnim;
     private Vector3 targetPosition;
 
 
@@ -16,6 +17,12 @@ public class Unit : MonoBehaviour
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            
+            unitAnim.SetBool("IsWalking", true);
+        }
+        else
+        {
+            unitAnim.SetBool("IsWalking", false);
         }
         
         if (Input.GetMouseButtonDown(0))
@@ -27,5 +34,6 @@ public class Unit : MonoBehaviour
     private void Move(Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
+        
     }
 }
