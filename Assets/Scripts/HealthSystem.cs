0abc53b8f0c3c8002b0972c479831410e23fb31c
TzvikaShtainer <TzvikaShtainer@gmail.com>
2024-04-20 12:38:11 +0300

    Unit World UI - Health UI

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -6,18 +6,27 @@ using UnityEngine;
 public class HealthSystem : MonoBehaviour
 {
     public event EventHandler OnDead;
+    public event EventHandler OnDamaged;
     
-    [SerializeField] private int health = 100;
+    [SerializeField] private int healthMax = 100;
+    [SerializeField] private int health;
 
+
+    private void Awake()
+    {
+        health = healthMax;
+    }
 
     public void Damage(int damageAmount)
     {
         health -= damageAmount;
-
+            
         if (health <= 0)
         {
             health = 0;
         }
+        
+        OnDamaged?.Invoke(this, EventArgs.Empty);
 
         if (health == 0)
         {
@@ -29,5 +38,9 @@ public class HealthSystem : MonoBehaviour
     {
         OnDead?.Invoke(this, EventArgs.Empty);
     }
-    
+
+    public float GetHealthNormalized()
+    {
+        return (float)health / healthMax;
+    }
 }
--- Assets/Scripts/UI/UnitWorldUI.cs
+++ Assets/Scripts/UI/UnitWorldUI.cs
@@ -3,18 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitWorldUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI actionPointsText;
     [SerializeField] private Unit Unit;
+    [SerializeField] private Image healthBarImage;
+    [SerializeField] private HealthSystem healthSystem;
 
 
     private void Start()
     {
         Unit.OnAnyActionPointsChanged += unit_OnAnyActionPointsChanged; //update every UnitWorldUI cuz the event is static, can be problem for 1000 units but for now its ok
+
+        healthSystem.OnDamaged += HealthSystem_OnDamaged;
         
         UpdateActionPointsText();
+        UpdateHealthBar();
     }
 
     private void UpdateActionPointsText()
@@ -25,5 +31,15 @@ public class UnitWorldUI : MonoBehaviour
     private void unit_OnAnyActionPointsChanged(object sender, EventArgs e)
     {
         UpdateActionPointsText();
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthBarImage.fillAmount = healthSystem.GetHealthNormalized();
+    }
+    
+    private void HealthSystem_OnDamaged(object sender, EventArgs e)
+    {
+        UpdateHealthBar();
     }
 }
