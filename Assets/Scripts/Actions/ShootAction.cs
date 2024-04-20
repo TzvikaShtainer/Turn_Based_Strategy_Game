b6a9899b13a71c42d02bab66b6d619d3be30e47d
TzvikaShtainer <TzvikaShtainer@gmail.com>
2024-04-20 14:24:28 +0300

    CameraEffect For Shooting

--- Assets/Scripts/Actions/ShootAction.cs
+++ Assets/Scripts/Actions/ShootAction.cs
@@ -101,8 +101,6 @@ public class ShootAction : BaseAction
 
     public override void TakeAction(GridPosition gridPosition, Action onActionComplete)
     {
-        ActionStart(onActionComplete);
-        
         targetUnit = LevelGrid.Instance.GetUnitAtGridPosition(gridPosition);
         
         state = State.Aiming;
@@ -110,6 +108,8 @@ public class ShootAction : BaseAction
         stateTimer = aimingStateTime;
 
         canShootBullet = true;
+        
+        ActionStart(onActionComplete);
     }
 
     public override List<GridPosition> GetValidActionGridPositionList()
@@ -150,5 +150,10 @@ public class ShootAction : BaseAction
         }
 
         return validGridPositionList;
+    }
+
+    public Unit GetTargetUnit()
+    {
+        return targetUnit;
     }
 }
